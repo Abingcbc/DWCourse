15d5abba81563dd11a597571c8c039cda1b471c0
Abingcbc <19990626.love@163.com>
2019-12-24 13:12:35 +0800

    final

--- final/app.py
+++ final/app.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from flask import request
-from hbase.v1.query import *
+from hbase.v1.query import queryMovieByTime, queryMovieByName, queryMovieByGenres
+from hbase.v2.query import queryMovieByPeople, \
+    queryMovieByRelation, queryMovieByScore, queryMovieBySentiment
 
 app = Flask(__name__, static_url_path="")
 
 keyword2Query = {
-    'year': queryMovieByTime
+    'year': queryMovieByTime,
+    'month': queryMovieByTime,
+    'week': queryMovieByTime,
+    'day': queryMovieByTime,
+    'quater': queryMovieByTime,
+    'name': queryMovieByName,
+    'director': queryMovieByPeople,
+    'actor': queryMovieByPeople,
+    'starring': queryMovieByPeople,
+    'relation': queryMovieByRelation,
+    'genres': queryMovieByGenres,
+    'score': queryMovieByScore,
+    'sentiment': queryMovieBySentiment
 }
 
 @app.route('/')
@@ -17,13 +31,19 @@ def query():
     count = 0
     result = ''
     query_map = {}
+    time = 0
     for key, value in request.args.items():
         print(str(key) + ': ' + str(value))
-        query_map[key] = value
-    result, count = keyword2Query['year'](query_map)
+        if key == 'season':
+            query_map['quater'] = value
+        else:
+            query_map[key] = value
+    if len(query_map) != 0:
+        result, count, time = keyword2Query[list(query_map.keys())[0]](query_map)
     return {
         'count': count,
-        'result': result
+        'result': result,
+        'time': time
     }
 
 
--- final/hbase/v1/schema.py
+++ final/hbase/v1/schema.py
@@ -4,60 +4,65 @@ import pandas as pd
 connection = happybase.Connection("localhost")
 print('connect HBase successfully\n')
 
-connection.create_table("movie", {"cf": dict()})
-connection.create_table("review", {"cf": dict()})
+# connection.create_table("movie", {"cf": dict()})
+# connection.create_table("review", {"cf1": dict()})
 print('create table successfully\n')
 
-movie_table = connection.table("movie")
+# movie_table = connection.table("movie")
 review_table = connection.table("review")
 
 movie_attr = {
-    "name": "name",
-    "star_score": "star",
-    "restrict_level": "restrict",
-    "buy_price": "buy",
-    "actors": "actors",
-    "directors": "directors",
-    "writers": "writers",
-    "producers": "producers",
-    "format": "format",
-    "language": "lan",
-    "number_of_tapes": "tapes",
-    "asin": "asin",
-    "region": "region",
-    "number_of_discs": "discs",
-    "studio": "studio",
+    "movie_name": "name",
+    "product_asin": "asin",
+    "score": "score",
+    "imdb": "imdb",
     "year": "year",
     "month": "month",
     "day": "day",
-    "week": "week",
-    "imdb_score": "imdb",
-    "time_len": "len",
-    "rent_price": "rent",
+    "quater": "quater",
+    "day_of_the_week": "week",
+    "directors": "ds",
+    "actors": "as",
+    "starring": "star",
+    "producers": "ps",
+    "language": "lan",
     "genres": "genres",
-    "starring": "starring",
-    "supporting_actors": "sactors",
-    "audio_languages": "alan",
-    "purchase_rights": "rights",
-    "subtitles": "subtitles",
-    "aspect_ratio": "ratio",
-    "dubbed": "dubbed",
-    "supporter":"supporter"
+    "studio": "studio",
+    "writers": "ws"
+}
+comment_attr = {
+    "movie_id": "id",
+    "summary": "sum",
+    "score": "score",
+    "time": "time",
+    "helpfulness": "help",
+    "user_id": "uid"
 }
 
-df = pd.read_csv('../../movies.csv', dtype=str)
-print('Begin insert')
+# df = pd.read_csv('../../movies.csv', dtype=str)
+# print('Begin insert movie')
+# count = 0
+# for index, row in df.iterrows():
+#     temp_map = {}
+#     for attr in movie_attr.keys():
+#         if pd.notna(row[attr]):
+#             temp_map['cf:'+movie_attr[attr]] = row[attr]
+#     movie_table.put(row['id'], temp_map)
+#     count += 1
+#     if count % 10000 == 0:
+#         print('----' + str(count) + '----')
+
+df = pd.read_csv('../../comments.csv', dtype=str)
+print('Begin insert comment')
 count = 0
 for index, row in df.iterrows():
     temp_map = {}
-    for attr in movie_attr.keys():
+    for attr in comment_attr.keys():
         if pd.notna(row[attr]):
-            temp_map['cf:'+movie_attr[attr]] = row[attr]
-    movie_table.put(row['id'], temp_map)
+            temp_map['cf1:'+comment_attr[attr]] = row[attr]
+    review_table.put(str(count), temp_map)
     count += 1
     if count % 10000 == 0:
         print('----' + str(count) + '----')
 
 connection.close()
-
-print('insert ' + str(count) + ' rows into HBase')
